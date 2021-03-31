489f0d4e619a49fdee1870d13c41a99302e1fde2
marvinero1 <marvinero@gmail.com>
2021-03-31 16:33:21 -0400

    ¨asd¨

--- src/app/calculadora/calculadora.page.ts
+++ src/app/calculadora/calculadora.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-calculadora',
@@ -6,10 +8,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.page.scss'],
 })
 export class CalculadoraPage implements OnInit {
+  public folder: string;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,private router: Router) { }
 
   ngOnInit() {
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  value = '0';
+  oldValue = '0';
+
+  lastOperator = 'x';
+  readyForNewInput = true;
+  numberGroups = [
+    [7, 8, 9, 'x'],
+    [4, 5, 6, '-'],
+    [1, 2, 3, '+'],
+    [0, 'c', '/', '=']
+  ];
+
+  onButtonPress(symbol) {
+    //console.log(symbol);
+
+    if (isNumber(symbol)) {
+      //console.log('is a number');
+      if (this.readyForNewInput)
+        this.value = '' + symbol;
+      else
+        this.value += '' + symbol;
+      this.readyForNewInput = false;
+    }
+    else if (symbol === 'c') {
+      this.value = '0';
+      this.readyForNewInput = true;
+    }
+    else if (symbol === '=') {
+      if (this.lastOperator === 'x')
+        this.value = '' + (parseInt(this.oldValue) * parseInt(this.value));
+      else if (this.lastOperator === '-')
+        this.value = '' + (parseInt(this.oldValue) - parseInt(this.value));
+      else if (this.lastOperator === '+')
+        this.value = '' + (parseInt(this.oldValue) + parseInt(this.value));
+      else if (this.lastOperator === '/')
+        this.value = '' + (parseInt(this.oldValue) / parseInt(this.value));
+      this.readyForNewInput = true;
+    }
+    else { // operator
+      this.readyForNewInput = true;
+      this.oldValue = this.value;
+      this.lastOperator = symbol;
+    }
+  }
+
+  perfil(){
+    this.router.navigate(['/perfil']);
+  }
 }
